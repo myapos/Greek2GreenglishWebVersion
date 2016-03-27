c9df6120d0e6e5d2ad7e8184ebf6d6e8e4be1cff
myapos <myapos@yahoo.com>
2016-03-27 13:18:35 +0300

    fixed slash and input text pass in convert params

--- app/components/Convert.js
+++ app/components/Convert.js
@@ -5,13 +5,18 @@ var Convert = React.createClass({
 
     getInitialState: function() {
         return {
-            alert: false 
+            alert: false,
+            inputtext:'' 
         };
     },
     componentDidMount: function(){
-        this.setState({alert: true});
+        this.setState({
+        	alert: true,
+         inputtext:this.props.params.inputtext
+        });
     },
     render: function() {
+    	console.log(this);
         var rsaOptions = {
             title: "Alert title",
             message: "This is an alert!",
@@ -23,6 +28,7 @@ var Convert = React.createClass({
                 }
             }
         };
+	        
         return (
             <div>
                 <ReactSimpleAlert options={rsaOptions} />
--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -9,12 +9,28 @@ var Link = ReactRouter.Link;
 
 var Prompt = React.createClass({
 
+    getInitialState: function() {
+        return {
+         inputtextvalue: 'Hello from input text!'
+        };
+    },
+   handleChange: function(event) {
+     this.setState({inputtextvalue: event.target.value});
+   },
+   
    render: function () {
+   	console.log(this);
     return (
       <div className="jumbotron col-sm-12 text-center" style={transparentBg}> 
-		<InputArea/>
+		<h3>Please enter input text to get output</h3>
+		<div className="form-group col-sm-5">
+  			<label>Input:</label>
+  			<textarea className="form-control" rows="5" value={this.state.inputtextvalue}
+			          onChange={this.handleChange}>
+			</textarea>
+		</div>
 		<div className="col-sm-2" > 
-			<Link to='/convert'>
+			<Link to={'convert/'+this.state.inputtextvalue}>
 			      <button
              			className="btn btn-block btn-success"
               			type="submit">
